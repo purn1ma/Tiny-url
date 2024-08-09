cb7bc1918515249ec1507d230b7d5e406001973a
purnima <purnimashrivastava97@gmail.com>
2024-08-09 22:23:02 +0530

    changed digest to base64

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -5,7 +5,7 @@ export async function encryption(data: string) {
   //   const secretKey = crypto.randomBytes(32).toString('hex');
   //   const iv = crypto.randomBytes(16)
     const cipher = crypto.createHash("sha256").update(data);
-    const digest = cipher.digest('hex');
+    const digest = cipher.digest('base64');
     const shortHash = digest.slice(0, 5)
     return shortHash;
   } catch (error) {
