679f5815ed7bc62f4bd5da7015936b2ac76fc7ac
purnima <purnimashrivastava97@gmail.com>
2024-08-09 20:26:01 +0530

    encryption logic

---
+++ backend/src/lib/utils.ts
@@ -0,0 +1,14 @@
+import * as crypto from "crypto"
+
+export async function encryption(data: string) {
+  try {
+  //   const secretKey = crypto.randomBytes(32).toString('hex');
+  //   const iv = crypto.randomBytes(16)
+    const cipher = crypto.createHash("sha256").update(data);
+    const digest = cipher.digest('hex');
+    const shortHash = digest.slice(0, 5)
+    return shortHash;
+  } catch (error) {
+    console.log(error)
+  }
+}
